5001487fc0177fe63088d24fce06bd7ed733412c
Narayan Aryal <aryal.coding@gmail.com>
2022-11-05 09:58:34 +1100

    more added

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,17 +3,32 @@ import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import { Button } from "react-bootstrap";
 import { MovieCard } from "./MovieCard";
+import { useState } from "react";
 
 export const SearchForm = () => {
+  const [form, setForm] = useState("");
+  // get the form data while
+
+  const handleOnChange = (e) => {
+    const { value } = e.target;
+    setForm(value);
+  };
+  // when form is submitted , call the api  with the search data and get the movie details
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
+
+  // display movie data in  our UI
   return (
-    <Form>
+    <Form className="py-3" onSubmit={handleOnSubmit}>
       <Row>
         <Col></Col>
         <Col>
-          <Form.Control placeholder="Movie name" />
+          <Form.Control onChange={handleOnChange} placeholder="Movie name" />
         </Col>
         <Col>
-          <Button> Search </Button>
+          <Button type="submit"> Search </Button>
         </Col>
       </Row>
       <Row className="py-3 justify-content-center">
