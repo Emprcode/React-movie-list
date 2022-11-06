71767df50c1a92631dc7d5496cca0955db3631a9
Narayan Aryal <aryal.coding@gmail.com>
2022-11-06 14:32:51 +1100

    more code added

--- src/utilities/AxiosHelpers.js
+++ src/utilities/AxiosHelpers.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 export const fetchData = (str) => {
     // console.log(str)
     // const apiEp = "https://www.omdbapi.com/?apikey=13d05e42&t=" + str;
-    const apiEp = `https://www.omdbapi.com/?apikey=13d05e42&t=${str}`;
+    const apiEp = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&t=${str}`;
     const response = axios(apiEp);
     return response;
 };
