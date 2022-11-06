6199dc772e1efa5f8ab649bd632cf086e26adddd
Narayan Aryal <aryal.coding@gmail.com>
2022-11-06 13:49:13 +1100

    more code added

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Title = () => {
   return (
     <div>
-      <h2 className="mt-5 text-center mb-2"> $$$ MY MOVIE LIST $$$</h2>
+      <h2 className="mt-5 text-center mb-2">MY MOVIE LIST</h2>
     </div>
   );
 };
