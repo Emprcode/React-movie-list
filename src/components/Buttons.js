b17a5c86139ef8dd8f8641bac7a9ee41cf4bcfd7
Narayan Aryal <aryal.coding@gmail.com>
2022-11-16 12:50:47 +1100

    code cleaned up

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -8,12 +8,8 @@ export const Buttons = ({ handleOnFilter }) => {
         <Button onClick={() => handleOnFilter("all")} variant="primary">
           All
         </Button>
-        <Button onClick={() => handleOnFilter("happy")} variant="success">
-        Happy
-        </Button>
-        <Button onClick={() => handleOnFilter("lazy")} variant="warning">
-        Lazy
-        </Button>
+        <Button onClick={() => handleOnFilter("happy")} variant="success">Happy</Button>
+        <Button onClick={() => handleOnFilter("lazy")} variant="warning">Lazy</Button>
       </ButtonGroup>
     </div>
   );
